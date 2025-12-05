79ec7994f3240381c2d143a47ac189fcab364461
Sala 2 <sala2@FI-LabSistemas.ucatolica.loc>
2025-12-05 08:10:00 -0500

    modelos

--- models.py
+++ models.py
@@ -1,12 +1,22 @@
+
+
 class Jugador():
     pass
 
 
-class Estadistica():
-    pass
+class Estadistica(BaseModel):
+    goles_marcados: int = 0
+    asistencias: int = 0
+    minutos_jugados: int = 0
+    tarjetas_amarillas: int = 0
+    tarjetas_rojas: int = 0
 
 
-class Partido():
-    pass
-
+class Partido(BaseModel):
+    rival: str
+    fecha: str 
+    resultado_local: int
+    resultado_visitante: int
+    goles_local: List[str] = [] 
+    goles_visitante: List[str] = [] 
 
