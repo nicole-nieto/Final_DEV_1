f2f4068f310fd0039c32065cd3d208f7914b82b2
Sala 2 <sala2@FI-LabSistemas.ucatolica.loc>
2025-12-05 09:11:41 -0500

    confi para la db

--- models.py
+++ models.py
@@ -1,35 +1,45 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 from utils.positions import Position
 from utils.states import States
 
 
-class Estadistica(BaseModel):
-    goles_marcados: int = 0
+class Estadistica(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    jugador_id: int = Field(foreign_key="jugador.id")
+    partido_id: int = Field(foreign_key="partido.id")
+
+    goles: int = 0
     asistencias: int = 0
-    minutos_jugados: int = 0
-    tarjetas_amarillas: int = 0
-    tarjetas_rojas: int = 0
+    minutos: int = 0
+    amarillas: int = 0
+    rojas: int = 0
+
+    jugador: "Jugador" = Relationship(back_populates="estadisticas_partido")
+    partido: "Partido" = Relationship(back_populates="estadisticas_jugadores")
 
 
-class Jugador(BaseModel):
-    nombre: str
-    fecha_nacimiento: int
+class Jugador(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str = Field(index=True, unique=True)
+    fecha_nacimiento: str
     numero_camiseta: int
-    nacionalidad: int
+    nacionalidad: str
     altura: int
     peso: int
     pie_dominante: str
     posicion: Position
     estado: States = States.ACTIVO
-    estadistica: Estadistica = Estadistica()
+
+    estadisticas_partido: List[Estadistica] = Relationship(back_populates="jugador")
 
 
-class Partido(BaseModel):
+class Partido(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     rival: str
     fecha: str
     resultado_local: int
     resultado_visitante: int
-    goles_local: List[str] = []
-    goles_visitante: List[str] = []
+    estado_partido: str = ""   
 
+    estadisticas_jugadores: List[Estadistica] = Relationship(back_populates="partido")
